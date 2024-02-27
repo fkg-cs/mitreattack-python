ef404cfef95f0123f44a03e79c204cd3057a0197
fkg-git <f.guarini9@studenti.uniba.it>
2024-02-27 21:26:20 +0100

    implementata recupero di tutte le campagne Enterprise, ICS e Mobile attribuite ai singoli gruppi. ICS e Mobile danno 0 risultati ma è corretto.

---
+++ fkg-cs/tree_campaigns_attributed_to_groups_enterprise.py
@@ -0,0 +1,25 @@
+from mitreattack.stix20 import MitreAttackData
+import pprint
+def main():
+    mitre_attack_data = MitreAttackData("enterprise-attack.json")
+
+    #ottengo tutti i gruppi a cui sono state attribuite tutte le campagne
+    groups_attributing = mitre_attack_data.get_all_groups_attributing_to_all_campaigns()
+
+    print(f"Retrived {len(groups_attributing.keys())} ENTERPRISE campaigns attributed to groups:")
+    i=0
+    #pprint.pprint(groups_attributing.items())
+    for id, groups in groups_attributing.items():
+        i=i+1
+        print(f"{i}) {id} was attributed to {len(groups)} {'group' if len(groups) == 1 else 'groups'} :")
+        # groups_attributing[id][0]['object'].name
+        #pprint.pprint(groups)
+        j=0
+        for group in groups:
+            j=j+1
+            external_group_id = group['object'].external_references[0].external_id
+            print(f"   {j}) {group['object'].name} (ATT&CK GROUP ID: {external_group_id})")
+
+
+if __name__ == "__main__":
+    main()
