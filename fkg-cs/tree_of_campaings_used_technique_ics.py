b1f7a9fc1455761c60c5cd9dfebdac781cc6c331
fkg-git <f.guarini9@studenti.uniba.it>
2024-02-29 16:14:28 +0100

    IMPLEMETATO ALBERO CHE RECUPERA TUTTE LE CAMPAGNE DI OGNI TIPOLOGIA: ENTERPRISE, ICS, MOBILE E LE CORRISPETTIVE TECNICHE UTILIZZATE
    NB:SNELLITO IL CODICHE GRAZIE ALL'IMPIEGO DEL METODO get_attack_id (evita di dover recuperare la lista di oggetti e fare i vari controlli con l'id strix)

---
+++ fkg-cs/tree_of_campaings_used_technique_ics.py
@@ -0,0 +1,33 @@
+from mitreattack.stix20 import MitreAttackData
+
+
+def main():
+    mitre_attack_data = MitreAttackData("json_matrix/ics-attack.json")
+    # ottengo tutte le campagne enterprise
+    campaigns = mitre_attack_data.get_campaigns(remove_revoked_deprecated=True)
+    i_campaign=0
+    n_campaigns=len(campaigns)
+    n_techniques=0
+
+    print("--------- TREE OF CAMPAIGNS ICS AND USED TECHNIQUES ---------")
+    for campaign in campaigns:
+        i_campaign=i_campaign+1
+        # id campagna su cui iterare
+        campaign_stix_id=campaign.id
+        techniques_used_by_campaign = mitre_attack_data.get_techniques_used_by_campaign(campaign_stix_id)
+
+        n_techniques=n_techniques+len(techniques_used_by_campaign)#aggiorno totale tecniche recueprate
+
+        print(f"{i_campaign}) Campaign '{campaign['name']}' has used {len(techniques_used_by_campaign)} techniques:")
+
+        i_technique=0#azzero contatore tecniche per ogni campagna
+        for technique in techniques_used_by_campaign:
+            i_technique=i_technique+1
+            #siccome questa volta il metodo restituisce un dizionario e non una semplice lista devo castare in oggetto
+            technique_object=technique['object']
+            print(f"        {i_technique}) {technique_object.name} (TECHNIQUE ATT&CK ID:{mitre_attack_data.get_attack_id(technique_object.id)}): {technique_object.description}\n")
+
+    print(f"\n --> REPORT: RETRIVED CAMPAIGNS: {n_campaigns} , TECHNIQUES RETRIVED: {n_techniques} <--\n")
+
+if __name__ == "__main__":
+    main()
