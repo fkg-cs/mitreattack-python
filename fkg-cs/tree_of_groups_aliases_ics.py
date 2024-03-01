d19cbbd807706af1a483e6e013e8ca0f8f89a521
fkg-git <f.guarini9@studenti.uniba.it>
2024-03-01 09:54:10 +0100

    formattato e migliorata leggibilità

--- fkg-cs/tree_of_groups_aliases_ics.py
+++ fkg-cs/tree_of_groups_aliases_ics.py
@@ -4,24 +4,25 @@ from mitreattack.stix20 import MitreAttackData
 def main():
     mitre_attack_data = MitreAttackData("json_matrix/ics-attack.json")
     groups = mitre_attack_data.get_groups(remove_revoked_deprecated=True)
-    n_aliases=0
+    n_aliases = 0
 
-    print(f"----- TREE OF GROUPS ALIASES -----")
+    print(f"----- TREE OF GROUPS ICS ALIASES -----")
     print(f"Retrieved {len(groups)} ATT&CK groups.")
 
     i = 0
     for group in groups:
-        i=i+1
+        i = i + 1
         print(f"{i}) GROUP: {group.description}")
         print(f"This group has {len(group.aliases)} alias:")
 
-        n_aliases=n_aliases+len(group.aliases)#aggiorno totale degli alias trovati
+        n_aliases = n_aliases + len(group.aliases)  # aggiorno totale degli alias trovati
         i_alias = 0
         for alias in group.aliases:
-            i_alias=i_alias+1
+            i_alias = i_alias + 1
             print(f"   {i_alias}) {alias}")
 
     print(f"\n --> REPORT: {len(groups)} GROUP FOUND , {n_aliases} ALIASES FOUND <--\n")
+
 
 if __name__ == "__main__":
     main()
