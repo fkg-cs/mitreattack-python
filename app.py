3537ec5d993521ad7a980565e6bf70db43dc6eb1
fkg-git <f.guarini9@studenti.uniba.it>
2024-03-10 22:32:22 +0100

    introdotta visualizzazione dei gruppi per ogni categoria di campagna

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+
+from fkg_cs.groups_list_web import get_groups_list
 from fkg_cs.tactics_and_techniques_relations_enterprise_web import get_enterprise_matrix
 from fkg_cs.tactics_and_techniques_relations_ics_web import get_ics_matrix
 from fkg_cs.tactics_and_techniques_relations_mobile_web import get_mobile_matrix
@@ -7,7 +9,10 @@ from fkg_cs.technique_page import get_technique_info
 app = Flask(__name__, static_folder=r'C:\Users\franc\Desktop\mitrepy\mitreattack-python\static',template_folder=r'C:\Users\franc\Desktop\mitrepy\mitreattack-python\templates')
 @app.route('/')
 def home_page():
-    render_template("home.html")
+    return render_template("home.html")
+@app.route('/matrix_selector')
+def matrix_selector_page():
+    return render_template("matrix_selector.html")
 @app.route('/matrix_enterprise')
 def matrix_enterprise():
     return render_template("matrix_template.html", output_list=get_enterprise_matrix())
@@ -22,5 +27,11 @@ def matrix_mobile():
 def technique_details(id):
     return render_template("technique_template.html", output_list=get_technique_info(id))
 
+@app.route('/CTI_groups')
+def tree_groups():
+    return render_template("groups_tree_template.html", output_list=get_groups_list())
+#@app.route('/CTI_groups/<id>', methods=['GET'])
+#def group_details(id):
+    #return render_template("group_template.html", output_list=get_group_info(id))
 if __name__ == "__main__":
     app.run()
